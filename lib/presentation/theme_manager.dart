2f7a13635886d32f6ac70588d2c4ed8825cbf22f
szy0syz <szy0syz@gmail.com>
2021-12-14 10:14:46 +0800

    update: add_card_theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,25 +1,30 @@
 import 'package:complete_advanced_flutter/presentation/color_manager.dart';
+import 'package:complete_advanced_flutter/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      // main colors of the app
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.primaryOpacity70,
-      primaryColorDark: ColorManager.darkPrimary,
-      // will be used incase of disabled button for example
-      disabledColor: ColorManager.grey1,
-      accentColor: ColorManager.grey
-      
-      // card view theme
+    // main colors of the app
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+    // will be used incase of disabled button for example
+    disabledColor: ColorManager.grey1,
+    accentColor: ColorManager.grey,
 
-      // App bar theme
+    // card view theme
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
 
-      // Button theme
+    // App bar theme
 
-      // Text theme
+    // Button theme
 
-      // input decoration theme (text form field)
+    // Text theme
 
-      );
+    // input decoration theme (text form field)
+  );
 }
