a8643d444135cf353da12e475ac4dc55efeb6234
szy0syz <szy0syz@gmail.com>
2021-12-14 10:20:20 +0800

    update: add_appbar_theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,30 +1,44 @@
-import 'package:complete_advanced_flutter/presentation/color_manager.dart';
-import 'package:complete_advanced_flutter/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
+
+import 'color_manager.dart';
+import 'styles_manager.dart';
+import 'values_manager.dart';
+import 'font_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-    // main colors of the app
-    primaryColor: ColorManager.primary,
-    primaryColorLight: ColorManager.primaryOpacity70,
-    primaryColorDark: ColorManager.darkPrimary,
-    // will be used incase of disabled button for example
-    disabledColor: ColorManager.grey1,
-    accentColor: ColorManager.grey,
+      // main colors of the app
+      primaryColor: ColorManager.primary,
+      primaryColorLight: ColorManager.primaryOpacity70,
+      primaryColorDark: ColorManager.darkPrimary,
+      disabledColor: ColorManager.grey1,
 
-    // card view theme
-    cardTheme: CardTheme(
-      color: ColorManager.white,
-      shadowColor: ColorManager.grey,
-      elevation: AppSize.s4,
-    ),
+      // ripple color
+      splashColor: ColorManager.primaryOpacity70,
 
-    // App bar theme
+      // will be used incase of disabled button for example
+      accentColor: ColorManager.grey,
 
-    // Button theme
+      // card view theme
+      cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4,
+      ),
 
-    // Text theme
+      // App bar theme
+      appBarTheme: AppBarTheme(
+          centerTitle: true,
+          color: ColorManager.primary,
+          elevation: AppSize.s4,
+          shadowColor: ColorManager.primaryOpacity70,
+          titleTextStyle: getRegularStyle(
+              color: ColorManager.white, fontSize: FontSize.s16))
 
-    // input decoration theme (text form field)
-  );
+      // Button theme
+
+      // Text theme
+
+      // input decoration theme (text form field)
+      );
 }
