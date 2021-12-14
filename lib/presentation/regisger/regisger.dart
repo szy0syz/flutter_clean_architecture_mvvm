b9e551b6385c0507c198a3bd418c037de6868255
szy0syz <szy0syz@gmail.com>
2021-12-14 10:43:22 +0800

    update: 重构代码结构

---
+++ lib/presentation/regisger/regisger.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegisgerView extends StatefulWidget {
+  RegisgerView({Key? key}) : super(key: key);
+
+  @override
+  _RegisgerViewState createState() => _RegisgerViewState();
+}
+
+class _RegisgerViewState extends State<RegisgerView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/store_details/store_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StoreDetailsView extends StatefulWidget {
+  StoreDetailsView({Key? key}) : super(key: key);
+
+  @override
+  _StoreDetailsViewState createState() => _StoreDetailsViewState();
+}
+
+class _StoreDetailsViewState extends State<StoreDetailsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
